93038e60769918f9801051efd208c2e30baec042
erikmetzinfo <erik.metz.info@gmail.com>
2021-03-05 17:41:35 -0500

    _

--- src/main.py
+++ src/main.py
@@ -7,58 +7,18 @@ from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 from typing import BinaryIO
-#import pdftotree
-#from pdfminer import pypdf2xml
+from bs4 import BeautifulSoup
 
+
+# Files
 BASE_DIR = abspath(dirname(__file__) )
-def get_files_as_array():
-    TDS_PATH = BASE_DIR + '/data/tds/'
-    return [f for f in listdir(TDS_PATH) if isfile(join(TDS_PATH, f))], TDS_PATH
+def get_files_as_array(datatype=""):
+    TDS_PATH = BASE_DIR + '/data/tds_pdf/'
+    return [f for f in listdir(TDS_PATH) if f.endswith(datatype) if isfile(join(TDS_PATH, f))], TDS_PATH
 
-def findInDict(needle, haystack):
-    for key in haystack.keys():
-        try:
-            value=haystack[key]
-        except:
-            continue
-        if key==needle:
-            return value
-        if isinstance(value,dict):            
-            x=findInDict(needle,value)            
-            if x is not None:
-                return x
-def get_raw_xml_pdf_data(filepath):
-    with open(filepath,'rb') as f:
-        pdf = pypdf.PdfFileReader(f)
-
-        # XFA
-        xfa = findInDict('/XFA',pdf.resolvedObjects)
-        xml = xfa[7].getObject().getData()
-
-
-    return xml
-
-def get_raw_acroforms_pdf_data(filepath):
-    with open(filepath,'rb') as f:
-        pdf = pypdf.PdfFileReader(f)
-
-        # Acroforms
-        acroforms = pdf.getFormTextFields()
-
-    return acroforms
-
-def get_html2(filepath):
-    if sys.version_info > (3, 0):
-        from io import StringIO
-    else:
-        from io import BytesIO as StringIO
-    from pdfminer.layout import LAParams
-    from pdfminer.high_level import extract_text_to_fp
-    output_string = StringIO()
-    with open(filepath, 'rb') as f:
-        a = extract_text_to_fp(f, output_string, laparams=LAParams(),output_type='html', codec=None)
-        b = 2
-def extract_text_from_pdf(pdf_fo: BinaryIO) -> str:
+# Pdf Extractor
+# https://pdfminersix.readthedocs.io/_/downloads/en/latest/pdf/
+def extract_text_from_pdf(pdf_fo: BinaryIO,output_type) -> str:
     """
     Extracts text from a PDF
 
@@ -67,16 +27,24 @@ def extract_text_from_pdf(pdf_fo: BinaryIO) -> str:
     :raises pdfminer.pdftypes.PDFException: on invalid PDF
     """
     out_fo = StringIO()
-    extract_text_to_fp(pdf_fo, out_fo, laparams=LAParams(), output_type='html', codec=None)
+    extract_text_to_fp(pdf_fo, out_fo, laparams=LAParams(), output_type=output_type, codec=None)
     return out_fo.getvalue()
-def get_html(filepath):
-    with open(filepath, 'rb') as f:
-        html_code = extract_text_from_pdf(f)
-        html_filename = filepath.replace('.pdf','.html').replace('tds_pdf','tds_html')
-        with open(html_filename, 'w+') as h:
-            h.write(html_code)
 
-        
+def save_file_as_html(filepath):
+    return __save_file_as_(filepath,'html')
+      
+def save_file_as_xml(filepath):
+    return __save_file_as_(filepath,'xml')
+
+def __save_file_as_(filepath,output_type):
+    extracted_filename = filepath.replace('.pdf',f'.{output_type}').replace('tds_pdf',f'tds_{output_type}')
+    with open(filepath, 'rb') as f:
+        extracted_code = extract_text_from_pdf(f,output_type)
+        with open(extracted_filename, 'w+') as h:
+            h.write(extracted_code)
+
+    return extracted_filename
+      
 def get_general_data_of_pdf(filepath):
     with open(filepath,'rb') as f:
         pdf = pypdf.PdfFileReader(f)
@@ -97,13 +65,82 @@ def get_general_data_of_pdf(filepath):
 
     return info
 
+# Analyze Html
+
+EXCLUDE_HEADERS = {'',' ','\n'}
+
+def is_nextPage(page_found, page):
+    if page_found:
+        page += 1
+        header_dict[page] = headers_per_page
+        headers_per_page = None
+        headers_per_page = set()
+        page_found = False
+
+def get_headers(div_container, headers, header_dict, page, page_found):
+    headers_per_page = set()
+
+    span_headers = div_container.find_all('span', {'style':'font-family: BookmanOldStyle-Bold; font-size:10px'})
+    for span_header in span_headers:
+        header = span_header.text
+        header = header.replace('\n','').strip()
+        if header not in EXCLUDE_HEADERS:
+            headers.add(header)
+            headers_per_page.add(header)
+            page_found = True
+
+    if page_found:
+        page += 1
+        header_dict[page] = headers_per_page
+        headers_per_page = None
+        headers_per_page = set()
+
+    return headers, header_dict, page, page_found
+
+def get_values(div_container):
+    span_parameters = div_container.find_all('span', {'style':'font-family: BookmanOldStyle-Bold; font-size:10px'})
+    if span_parameters != []:
+        a = 1
+    for span_parameter in span_parameters:
+        parameter = span_parameter.text
+        parameter = parameter.replace('\n','').strip()
+        if parameter not in EXCLUDE_HEADERS:
+            a = 1
+
+def get_headers_from_soup(soup):
+    headers = set()
+    header_dict = dict()
+    
+    div_header_style_tag = 'position:absolute; border: textbox 1px solid; writing-mode:lr-tb;'
+    div_header_containers = soup.find_all('div',style=lambda value: value and div_header_style_tag in value)
+    
+    page = 1
+    page_found = False
+    for div_header_container in div_header_containers:
+        headers, header_dict, page, page_found = get_headers(div_header_container, headers, header_dict, page, page_found)
+        #get_values(div_header_container)
+        if page_found:
+
+            page_found = False
+        
+    return headers, header_dict
+
+def get_dict_from_html(filename):
+    headers = set()
+    header_dict = dict()
+    with open(filename, 'rb') as f:
+        soup = BeautifulSoup(f,'html.parser')
+        headers, header_dict = get_headers_from_soup(soup)
+
+    return headers, header_dict
+
 def main():
-    filenames, tds_path = get_files_as_array()
+    filenames, tds_path = get_files_as_array(datatype=".pdf")
     for filename in filenames:
         print(tds_path + filename)
-        #a = pdftotree.parse(filename, html_path=None, model_type=None, model_path=None, visualize=False)
-        #a = pypdf2xml(tds_path + filename)
-        info = get_html(tds_path + filename)
+        html_filename = save_file_as_html(tds_path + filename)
+        # xml_filename = save_file_as_xml(tds_path + filename)
+        headers, header_dict = get_dict_from_html(html_filename)
 
 if __name__ == '__main__':
     main()
